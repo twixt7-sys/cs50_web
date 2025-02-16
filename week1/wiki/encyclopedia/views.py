87a56d1085645c2272b268095d1b53243c55645b
twixt7-sys <nexushellstein@gmail.com>
2025-02-16 21:55:25 +0800

    initialized book

--- week1/wiki/encyclopedia/views.py
+++ week1/wiki/encyclopedia/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponseBadRequest
 
 from . import util
 
-import markdown2 as md2
-import random as r
+import markdown2
 
 
 def index(request):
@@ -14,16 +13,15 @@ def index(request):
     })
 
 def entry(request, title):
-    entry = util.get_entry(title)
-    if entry == None:
+    if util.get_entry(title) == None:
         return render(request, "encyclopedia/error.html", {
             "error": "The page you are looking for does not exist."
         })
     
     return render(request, "encyclopedia/entry.html", {
-        "entry": entry,
+        "entry": util.get_entry(title),
         "title": title,
-        "content": md2.markdown(entry)
+        "content": markdown2.markdown(util.get_entry(title))
     })
     
 def search(request):
@@ -39,7 +37,7 @@ def search(request):
         })
     else:
         return render(request, "encyclopedia/search.html", {
-            "results": [],
+            "results": [],  #return results as an empty list if no query is provided
             "query": query
         })
 
@@ -57,28 +55,26 @@ def newpage(request):
         else:
             # save the new page
             util.save_entry(new_title, new_content)
-            entry = util.get_entry(new_title)
             return render(request, "encyclopedia/entry.html", {
-                "entry": entry,
+                "entry": util.get_entry(new_title),
                 "title": new_title,
-                "content": md2.markdown(entry)
+                "content": new_content
             })
     else:
         return render(request, "encyclopedia/newpage.html")
 
 def edit(request, title):
-    entry = util.get_entry(title)
-    
     if request.method == "POST":
         new_title = request.POST.get("title")
         new_content = request.POST.get("content")
         util.save_entry(new_title, new_content)
         return render(request, "encyclopedia/entry.html", {
-            "entry": entry,
+            "entry": util.get_entry(new_title),
             "title": new_title,
-            "content": md2.markdown(entry)
+            "content": new_content
         })
     
+    entry = util.get_entry(title)
     if entry is None:
         return render(request, "encyclopedia/error.html")
 
@@ -86,18 +82,4 @@ def edit(request, title):
         "entry": entry,
         "title": title,
         "content": entry
-    })
-    
-def random(request):
-    entries = util.list_entries()
-    if not entries:
-        return render(request, "encyclopedia/error.html", {
-            "error": "No entries available."
-        })
-    random_title = r.choice(entries)
-    entry = util.get_entry(random_title)
-    return render(request, "encyclopedia/entry.html", {
-        "entry": entry,
-        "title": random_title,
-        "content": md2.markdown(entry)
     })
