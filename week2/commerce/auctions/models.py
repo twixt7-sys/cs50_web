d918f159f7e5900b83c349064d9b9829a6b33613
twixt7-sys <nexushellstein@gmail.com>
2025-02-17 23:00:02 +0800

    auction models progression

--- week2/commerce/auctions/models.py
+++ week2/commerce/auctions/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import models as m
 
+#classes usually contains string representations of the objects
+class User(AbstractUser): # fields: username, password, email
+    # using max_lengths based on convention
+    username = m.CharField(max_length=150, unique=True)
+    password = m.CharField(max_length=128)
+    email = m.EmailField(max_length=254, unique=True)
+    
+    def __str__(self):
+        return f"username: {self.username}\nemail: {self.email}\npassword: {self.password}"
 
-class User(AbstractUser):
-    pass
+class Listing(m.Model): # fields: name, price, description, date
+    name = m.CharField(max_length=150)
+    price = m.DecimalField(max_digits=10, decimal_places=2)
+    description = m.TextField()
+    date = m.DateTimeField(auto_now_add=True)
+    image_url = m.URLField()
+    category = m.CharField(max_length=150)
+
+class Bid(m.Model): # fields: amount, bidder, listing
+    amount = m.DecimalFiel(max_length=10, decimal_places=2)
+    bidder = m.ForeignKey(User, on_delete=m.CASCADE, related_name="bids")
+    listing = m.ForeignKey(Listing, on_delete=m.CASCADE, related_name="bids")
+
+class Comment(m.Model): # fields: content, commenter, listing
+    content = m.TextField()
+    commenter = m.ForeignKey(User, on_delete=m.CASCADE, related_name="comments")
+    listing = m.ForeignKey(Listing, on_delete=m.CASCADE, related_name="comments")
