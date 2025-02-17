ccc21eb36b82ae8e7be5b9549e4ba24f41f8cf5a
twixt7-sys <nexushellstein@gmail.com>
2025-02-17 08:35:24 +0800

    initialized project2

--- week2/airline/flights/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('<str:flight_id>', views.flight, name="flight"),
-    path('<str:flight_id>/book', views.book, name="book")
-]
