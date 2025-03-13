4f3112b7e069d89b2c1223a329d52abaac38070e
twixt7-sys <nexushellstein@gmail.com>
2025-03-13 12:01:43 +0800

    cleaned and documented models

--- week2/project2/commerce/auctions/models.py
+++ week2/project2/commerce/auctions/models.py
@@ -4,19 +4,23 @@ from django.utils import timezone
 
 class User(AbstractUser):
     watchlist = m.ManyToManyField("Listing", blank=True, related_name="watchers")
-    
-    def __str__(self):
+    #primary attributes: username, password, email, first_name, last_name
+    def __str__(self):  #returns this string when printed
         return f"username: {self.username}, email: {self.email}"
 
 class Listing(m.Model):
     id = m.AutoField(primary_key=True)
+    
     is_active = m.BooleanField(default=True)    
-    user = m.ForeignKey(User, on_delete=m.CASCADE, related_name="listings")
     name = m.CharField(max_length=150)
     description = m.TextField()
     date = m.DateTimeField(auto_now_add=True)
     image_url = m.URLField(null=True, blank=True)
     category = m.CharField(max_length=150, null=True, blank=True)
+    
+    user = m.ForeignKey(User, on_delete=m.CASCADE, related_name="listings")
+    
+    #Uses string if referenced from a class declared below the this clas
     starting_bid = m.ForeignKey('Bid', null=True, blank=True, on_delete=m.SET_NULL, related_name="starting_for")  
     highest_bid = m.ForeignKey('Bid', null=True, blank=True, on_delete=m.SET_NULL, related_name="highest_for")
     
@@ -27,7 +31,6 @@ class Listing(m.Model):
         if is_new and not self.starting_bid:
             default_bid = Bid.objects.create(amount=0, bidder=self.user, listing=self)
             Listing.objects.filter(pk=self.pk).update(starting_bid=default_bid, highest_bid=default_bid)
-
 
 
     def __str__(self):
